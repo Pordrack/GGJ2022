3a68a9c5939a949bc21921ce884fca81fa125a4d
Obiasnara <74296002+Obiasnara@users.noreply.github.com>
2022-01-30 14:23:05 +0100

    Death bug

--- Assets/ApplyDamage.cs
+++ Assets/ApplyDamage.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ApplyDamage : MonoBehaviour
 {
+    public GameObject roger;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,11 @@ public class ApplyDamage : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player")) {
+            roger.GetComponent<Respawn>().Die();
+        }
     }
 }
